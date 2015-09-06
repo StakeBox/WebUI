73d2aa18ec691af2b0dc21df4fceba00e56d66f1
StakeBox <support@stakebox.com>
2015-09-05 22:56:10 -0400

    v1.0.4

--- WebUI/footer.php
+++ WebUI/footer.php
@@ -1,5 +1,5 @@
 			<footer>
-				<a href="http://stakebox.com">StakeBox WebUI v1.0.4</a>
+				<a href="http://stakebox.com">StakeBox WebUI v1.0.4.2</a>
 			</footer>
 		</div><!--.container-->
 </pre></body></html>
