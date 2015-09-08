f8dd2b360bde04866d40be08d5d9aae31e121b66
StakeBox <support@stakebox.com>
2015-09-07 23:37:40 -0400

    v1.0.6

--- WebUI/encryptwallet.php
+++ WebUI/encryptwallet.php
@@ -28,7 +28,7 @@ function changeLockState(){
 
 try {
 	$coin->encryptwallet($password);
-		echo "<p class='bg-success'><b>Your wallet is now encrypted, your password is $password, do not forget it, there is no way of recovering it. This process will restart the wallet, it will take a few minutes before you can access it again.</b></p> ";
+		echo "<p class='bg-success'><b>Your wallet is now encrypted, your password is $password, do not forget it, there is no way of recovering it. This process will stop the wallet, you must restart your StakeBox before you can access it again.</b></p> ";
 		$newLockState = "Locked";
                 changeLockState();
 	} catch(Exception $e) {
