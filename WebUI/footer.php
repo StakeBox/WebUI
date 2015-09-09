a868f63733275dc3da440ccdb908f0ed2abc2d3b
StakeBox <support@stakebox.com>
2015-09-09 16:11:07 -0400

    fixed lock file location

--- WebUI/footer.php
+++ WebUI/footer.php
@@ -1,5 +1,5 @@
 			<footer>
-				<a href="http://stakebox.com">StakeBox WebUI v1.1.0</a>
+				<a href="http://stakebox.com">StakeBox WebUI v1.1.1</a>
 			</footer>
 		</div><!--.container-->
 </pre></body></html>
