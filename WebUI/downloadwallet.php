f0135d00f47947c2f7095e25e875478ca6904185
StakeBox <support@stakebox.com>
2015-11-08 19:37:15 -0500

    v1.2.2

--- WebUI/downloadwallet.php
+++ WebUI/downloadwallet.php
@@ -4,7 +4,7 @@ include ("pass.php");
 $destination = "/var/www/html/webui/WebUI/".$currentWallet."wallet.dat";
 $coin->backupwallet($destination);
 exec("sudo chmod g+r /var/www/html/webui/WebUI/".$currentWallet."wallet.dat");
-header("Location: http://stakebox.local/".$currentWallet."wallet.dat");
+header("Location: http://".$_SERVER['SERVER_NAME']."/".$currentWallet."wallet.dat");
 exit;
 ?>
 
